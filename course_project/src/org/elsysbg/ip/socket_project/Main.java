31d96eaee897cda372b638915da17f32711f3562
3phase <pbozhinov@yahoo.com>
2015-11-15 23:00:37 +0200

    Adding new class Reference and removing additional logics from Parse

--- course_project/src/org/elsysbg/ip/socket_project/Main.java
+++ course_project/src/org/elsysbg/ip/socket_project/Main.java
@@ -10,10 +10,13 @@ public class Main {
 		Map<String, String> userMeta = new HashMap<>();
 		Parse parser = new Parse();
 		Scanner in = new Scanner(System.in);
+		Reference r = new Reference();
+		
 		while (in.hasNextLine()) {
 			userMeta = parser.parse(in.nextLine());
 			if ("shutdown" == userMeta.get("command"))
 				break;
+			r.make_reference(userMeta);
 		}
 		
 		in.close();
