77791954c331b506d024568202f51e94b5240839
3phase <pbozhinov@yahoo.com>
2015-11-17 00:51:24 +0200

    Updated and tested communication between User and log-class Interval

--- course_project/src/org/elsysbg/ip/socket_project/User.java
+++ course_project/src/org/elsysbg/ip/socket_project/User.java
@@ -1,10 +1,15 @@
 package org.elsysbg.ip.socket_project;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class User {
 	
 	private String username;
 	private Integer count_of_entries = 0;
 	private Integer currently_logged = 0;
+	private List<Interval> visit_interval = new ArrayList<Interval>();
 	
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -24,11 +29,20 @@ public class User {
 	
 	public void logout() {
 		this.currently_logged = 0;
+		visit_interval.get(visit_interval.size()-1).setTo(new Date());
 	}
 	
 	public void login() {
 		this.currently_logged = 1;
 		this.count_of_entries += 1;
+		visit_interval.add(new Interval(new Date()));
+	}
+	
+	public void getAllIntervals() {
+		for (Interval interval : visit_interval) {
+			System.out.print(":"+interval.getFrom());
+			System.out.print(":"+interval.getTo());
+		}
 	}
 	
 }
